7efa02bda70b311449ae5d3cbe1976823a6746f8
wleeeeeeeeeee <wlee94work@gmail.com>
2024-12-17 18:46:23 +0900

    Adding some onnx engine functionality.

--- src/Inference_Engine/Frameworks/OnnxInferenceEngine.h
+++ src/Inference_Engine/Frameworks/OnnxInferenceEngine.h
@@ -8,19 +8,74 @@ private:
 	Ort::AllocatorWithDefaultOptions allocator{};
 	Ort::RunOptions runOptions{};
 	Ort::Session session = Ort::Session(nullptr);
+	size_t input_nums{};
+	size_t output_nums{};
+	vector<const char*> input_node_names;
+	vector<const char*> output_node_names;
+
+	void get_model(std::string& model_path, std::string& device, int threads = 0, int gpu_mem_limit = 4) {
+		try {
+			auto availableProviders = Ort::GetAvailableProviders();
+			for (const auto& provider : availableProviders) {
+				std::cout << provider << " ";
+			}
+			std::cout << std::endl;
+
+			Ort::SessionOptions sessionOptions;
+			sessionOptions.SetIntraOpNumThreads(threads);
+			sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
+
+			//cuda
+			OrtCUDAProviderOptions cuda_options;
+			cuda_options.device_id = 0;
+			cuda_options.arena_extend_strategy = 0;
+			cuda_options.gpu_mem_limit = (size_t)gpu_mem_limit * 1024 * 1024 * 1024; // gpu memory limit
+			cuda_options.cudnn_conv_algo_search = OrtCudnnConvAlgoSearch::OrtCudnnConvAlgoSearchExhaustive;
+			cuda_options.do_copy_in_default_stream = 1;
+			sessionOptions.AppendExecutionProvider_CUDA(cuda_options);
+
+			wchar_t* model_path1 = new wchar_t[model_path.size()];
+			swprintf(model_path1, 4096, L"%S", model_path.c_str());
+
+			this->session = Ort::Session(this->env, model_path1, sessionOptions);
+		}
+		catch (std::exception ex) {
+			// somekind of exception error printed on log
+		}
+	}
+
+	void get_onnx_info() {
+
+	}
+
 public:
 	void loadModel(const std::string& modelPath) override {
-		//load model into session
-		//configure gpu, cpu, any necessary inference options
+		//load model into session + configure gpu, cpu, any necessary inference options
+		get_model(modelPath, "cuda");
+		
 		//get onnx info (node names, input shape, output shape etc)
+		get_onnx_info();
+
 	}
 
 	void infer(const typename TaskType::InputType& input, typename TaskType::OutputType& output) override {
-		//prepare input tensor
-
-		//run inference
-
-		//task-specific postprocessing
-		TaskType::processOuput(rawOutput, output);
+		try {
+			//prepare input tensor
+			TaskType::preProcess();
+			//run inference
+			output_tensors = session.Run(
+				this->runOptions,
+				this->input_node_names.data(),
+				&input_tensor,
+				this->input_nums,
+				this->output_node_names.data(),
+				this->output_nums
+			);
+			//task-specific postprocessing
+			TaskType::processOuput(rawOutput, output);
+		}
+		catch (std::exception& ex) {
+			// somekind of exception error printed on log
+		}
 	}
 };
